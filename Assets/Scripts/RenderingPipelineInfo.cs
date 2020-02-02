51432c5ceead04248508006bc2c4190f872e74e8
Dilmer Valecillos <dilmervst@gmail.com>
2020-02-02 11:46:46 -0700

    Converted AR Project from standard pipeline to universal pipeline

---
+++ Assets/Scripts/RenderingPipelineInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class RenderingPipelineInfo : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI pipelineInfo;
+    
+    void Start()
+    {
+        pipelineInfo.text = $"Current Rendering Pipeline: {GraphicsSettings.currentRenderPipeline.name}";    
+    }
+}
