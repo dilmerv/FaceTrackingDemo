258d7cdd6a36ffc7556012099d006afad05ec581
Dilmer Valecillos <dilmervst@gmail.com>
2020-03-24 02:39:51 -0600

    Added a new demo where I show you how to select dynamic face textures

---
+++ Assets/ToggleFace.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ToggleFace : MonoBehaviour
+{
+    public ARFaceManager arFaceManager;
+
+    public GameObject[] faces;
+
+    private float timer;
+
+    [SerializeField]
+    private float frequency = 1.0f;
+
+    [SerializeField]
+    private float maxUntilSpawn = 1.0f;
+
+    private int index = -1;
+
+    void Update()
+    {
+        if (timer <= maxUntilSpawn)
+        {
+            timer += Time.deltaTime * frequency;
+        }
+        else
+        {
+            if(index + 1 == faces.Length)
+            {
+                index = -1;
+            }
+            index++;
+
+
+            if(arFaceManager != null)
+                DestroyImmediate(arFaceManager);
+            
+            arFaceManager = gameObject.AddComponent<ARFaceManager>();
+            arFaceManager.facePrefab = faces[index];
+
+            timer = 0;
+            Debug.Log(index);
+        }
+    }
+}
